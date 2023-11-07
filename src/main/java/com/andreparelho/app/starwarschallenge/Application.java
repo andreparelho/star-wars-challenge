df48d9218d397f711c790efafae529b0f482940e
Andre <parelhoneves.andre@gmail.com>
2023-11-07 20:04:59 -0300

    inital commit

---
+++ src/main/java/com/andreparelho/app/starwarschallenge/Application.java
@@ -0,0 +1,13 @@
+package com.andreparelho.app.starwarschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
