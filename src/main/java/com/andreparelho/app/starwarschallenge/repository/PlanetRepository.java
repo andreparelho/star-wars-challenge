9871b92c88e61040ce06c94f1288e55a2c601d57
Andre <parelhoneves.andre@gmail.com>
2023-11-08 21:49:19 -0300

    create reposotiry interface methods

--- src/main/java/com/andreparelho/app/starwarschallenge/repository/PlanetRepository.java
+++ src/main/java/com/andreparelho/app/starwarschallenge/repository/PlanetRepository.java
@@ -1,4 +1,14 @@
 package com.andreparelho.app.starwarschallenge.repository;
 
+import com.andreparelho.app.starwarschallenge.entity.PlanetEntity;
+
+import java.util.List;
+
 public interface PlanetRepository {
+    PlanetEntity createPlanet(PlanetEntity planetEntity);
+    List<PlanetEntity> listPlanets();
+    PlanetEntity getPlanetByName(String name);
+    PlanetEntity getPlanetById(Long id);
+    String deletePlanetByName(String name);
+    String deletePlanetById(Long id);
 }
--- src/main/java/com/andreparelho/app/starwarschallenge/repository/PlanetRepositoryImpl.java
+++ src/main/java/com/andreparelho/app/starwarschallenge/repository/PlanetRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.andreparelho.app.starwarschallenge.repository;
 
+import com.andreparelho.app.starwarschallenge.model.response.PlanetModelResponse;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PlanetRepositoryImpl {
+
 }
